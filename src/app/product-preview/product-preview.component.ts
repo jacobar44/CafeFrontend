eab119d517aa7c468ab797940068e8db20ccb631
jacobar44 <jacobreed44@gmail.com>
2023-04-11 22:04:57 -0400

    adding products

---
+++ src/app/product-preview/product-preview.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input,OnInit } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product';
+import { ProducrService } from '../producr.service';
+
+@Component({
+  selector: 'app-product-preview',
+  templateUrl: './product-preview.component.html',
+  styleUrls: ['./product-preview.component.css']
+})
+export class ProductPreviewComponent implements OnInit {
+
+  @Input() product!:Product;
+  constructor(
+    private routes: ActivatedRoute,
+    private producrService: ProducrService,
+    private fb: FormBuilder,
+    private router: Router
+    
+  ) {}
+
+  ngOnInit(): void {
+    this.producrService
+      .getProductById(parseInt(this.routes.snapshot.paramMap.get('id')!))
+      .subscribe((response) => (this.product = response));
+  }
+
+}
---
+++ src/app/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+    constructor(
+        public pid: number,
+        public name: string,
+        public price: number,
+        public image: string,
+        public ingredients: string,
+        public description: string
+    ){}
+}
